e493eb951366a3fcfb35d60cf6187d27f5bf9854
huai-sian <bpud50122@gmail.com>
2022-05-10 22:47:40 +0800

    update

--- src/components/PaymentFooter.vue
+++ src/components/PaymentFooter.vue
@@ -1,7 +1,7 @@
 <template>
     <div class='footer'>
         <footer>
-            <p>Copy right Company 2020</p>
+            <p>Copy right Company 2022</p>
         </footer>
     </div>
 </template>
